ee91c38af0e2460fe7ef6b8f421c78ecf85edc48
MaxSpanier <maxspanier17@gmail.com>
2020-05-13 13:59:54 +0200

    Modified it

--- Reverse_String/reverse_string.py
+++ Reverse_String/reverse_string.py
@@ -1,3 +1,5 @@
+import sys
+
 class ReverseString():
     def __init__(self, string):
         self.string = string
@@ -9,7 +11,15 @@ class ReverseString():
         return given_string[::-1]
 
     def play_again(self):
-        pass
+        choice = str(input("-------------------------------\nDo you want to reverse another string? (y/n) - "))
+        if choice == "y":
+            self.string = ""
+            self.main()
+        elif choice == "n":
+            sys.exit()
+        else:
+            print("Plese enter a valid answer.")
+            self.play_again()
 
     def main(self):
         if self.string == "":
@@ -18,8 +28,10 @@ class ReverseString():
         reversed_string = self.reverse(self.string)
         print(f"Your given string reversed looks like this: \n{reversed_string}")
 
-reverser = ReverseString("Hallo")
-reverser.main()
+        self.play_again()
+
+# reverser = ReverseString("Hallo")
+# reverser.main()
 
 reverser02 = ReverseString("")
 reverser02.main()
